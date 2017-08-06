6c870bb5408b7cf44bd9a0e34d07ca5d0d7847ae
florcava <florcava@outlook.com>
2017-08-06 13:45:51 +0800

    Using GET

--- headlines.py
+++ headlines.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 import feedparser
 
@@ -13,8 +14,12 @@ RSS_FEEDS = {
     }
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication='bbc'):
+def get_news():
+    query = request.args.get('publication')
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = 'bbc'
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html", articles=feed['entries'])
 
